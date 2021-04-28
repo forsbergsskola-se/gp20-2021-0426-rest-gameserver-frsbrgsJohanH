8b519b9a571ecd9025c23a5f6604c30c7cb29d59
Johan Hammarström <johan.hammarstrom@student.forsbergsskola.se>
2021-04-28 12:24:06 +0200

    Setting up my project.

---
+++ TinyBrowser/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace TinyBrowser
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
