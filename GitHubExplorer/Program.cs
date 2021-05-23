654406fc4570715667649df760d1b8b49403de1a
Johan Hammarström <johan.hammarstrom@student.forsbergsskola.se>
2021-05-23 19:29:03 +0200

    setting up github project

---
+++ GitHubExplorer/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace GitHubExplorer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            
+            //token:  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
+        }
+    }
+}
