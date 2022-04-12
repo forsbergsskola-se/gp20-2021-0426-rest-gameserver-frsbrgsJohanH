580b185bec85fab24f570392b494dd0da2231ab8
Johan Hammarström <johan.hammarstrom@student.forsbergsskola.se>
2022-04-13 01:02:56 +0200

    Reads the response to my request as a string
    Try to deserialize with Json

--- GitHubExplorer/Program.cs
+++ GitHubExplorer/Program.cs
@@ -1,6 +1,11 @@
 ﻿using System;
+using System.IO;
 using System.Net;
 using System.Net.Http;
+using System.Net.Http.Json;
+using System.Text;
+using System.Text.Json;
+using System.Text.Json.Serialization;
 using System.Threading.Tasks;
 
 namespace GitHubExplorer
@@ -13,7 +18,11 @@ namespace GitHubExplorer
         public static string gitAPI = "https://api.github.com/users/frsbrgsJohanH";
 
         public static HttpRequestMessage RequestMessage;
-        public static string response;
+        
+
+        public static StreamReader StreamReader;
+
+        
         
         
         
@@ -22,7 +31,7 @@ namespace GitHubExplorer
         {
             Console.WriteLine("Hello World!");
 
-            Task.WaitAll(Ececute());
+            Task.WaitAll(Execute());
             Console.ReadLine();
 
 
@@ -31,19 +40,41 @@ namespace GitHubExplorer
 
         }
 
-        public static async Task Ececute()
+        public static async Task Execute()
         {
             Client = new HttpClient();
+            
 
             Client.BaseAddress = new Uri("https://api.github.com");
             
             Client.DefaultRequestHeaders.UserAgent.Add(new System.Net.Http.Headers.ProductInfoHeaderValue("AppName", "1.0"));
-            Client.DefaultRequestHeaders.Accept.Add(new System.Net.Http.Headers.MediaTypeWithQualityHeaderValue("application/json"));
+            Client.DefaultRequestHeaders.Accept.Add(new System.Net.Http.Headers.MediaTypeWithQualityHeaderValue("application/vnd.github.v3+json"));
             Client.DefaultRequestHeaders.Authorization = new System.Net.Http.Headers.AuthenticationHeaderValue(Secret.token);
 
-            var response = await Client.GetAsync("/users/frsbrgsJohanH");
+            var response = await Client.GetAsync("/users/frsbrgsJohanH/repos");
+            response.EnsureSuccessStatusCode();
+
+            var content = await response.Content.ReadAsStringAsync();
+            
+            Console.WriteLine(content);
+
+          
+            
+            
+            
+
+            response.Content.ReadAsStream();
+
+            /*UserResponse? userResponse = await Client.GetFromJsonAsync<UserResponse>("/users/frsbrgsJohanH/");
+            Console.WriteLine($"Name: {userResponse.name}");
+            Console.WriteLine($"Name: {userResponse.job}");*/
+
+            //UserResponse? userResponse = JsonSerializer.Deserialize<UserResponse>(jsonText);
+            
             
             Console.WriteLine(response);
+
+            
 
         }
         
---
+++ GitHubExplorer/UserResponse.cs
@@ -0,0 +1,8 @@
+﻿namespace GitHubExplorer
+{
+    public class UserResponse
+    {
+        public string name { get; set; }
+        public string job { get; set; }
+    }
+}
